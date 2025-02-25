93808c2b7aeca31b39138270de30ce2f86e2b0d0
jack <134116173+wangxiaobai08@users.noreply.github.com>
2025-02-25 21:49:32 +0800

    Delete project directory

--- project/example/main.js
+++
@@ -1,5 +0,0 @@
-const {vm_run}=require(`../../utils/vm_run.js`)
-inputpath=`D:\\SourceInsight\\逆向风控\\爬虫艺术\\过验证码\\FixEnv\\project\\example\\input.js`
-outputpath=`D:\\SourceInsight\\逆向风控\\爬虫艺术\\过验证码\\FixEnv\\project\\example\\output.js`
-
-vm_run(inputpath, outputpath);
